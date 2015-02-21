f82f8116df659f88e0941553db1f20d31fb8549f
Josh Chisholm <joshuachisholm@gmail.com>
2015-02-21 16:31:27 +0000

    30s timeout for really slow browsers :(

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -50,7 +50,7 @@ module.exports = function(config) {
     customLaunchers: browsers,
     captureTimeout: 120000,
     singleRun: true,
-    browserNoActivityTimeout: 20000,
+    browserNoActivityTimeout: 30000,
     sauceLabs: {
       startConnect: !process.env.TRAVIS_JOB_NUMBER,
       tunnelIdentifier: process.env.TRAVIS_JOB_NUMBER || (new Date().getTime())
