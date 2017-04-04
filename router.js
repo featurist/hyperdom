ea5f6aeb9051dd7099597d47705c7307f01702fe
Tim Macfarlane <timmacfarlane@gmail.com>
2017-04-04 06:46:07 +0100

    router allows components as routes

--- router.js
+++ router.js
@@ -378,10 +378,19 @@ function findMatch(url, routes) {
     var route = routes[r];
     var match
 
-    if ((match = route.match(url))) {
-      return {
-        route: route,
-        match: match
+    if (typeof route.match === 'function') {
+      if ((match = route.match(url))) {
+        return {
+          route: route,
+          match: match
+        }
+      }
+    } else if (typeof route.routes === 'function') {
+      var subRoutes = modelRoutes(route)
+      var subMatch = findMatch(url, subRoutes)
+      
+      if (subMatch) {
+        return subMatch
       }
     }
   }
