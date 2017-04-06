1a262f5d0dcc7f29ac08e36ba9e50f53cb1afbcf
Tim Macfarlane <timmacfarlane@gmail.com>
2017-04-06 08:58:15 +0100

    fixed require hyperdom module, slightly better exception handling

--- hx.js
+++ hx.js
@@ -1,9 +1,10 @@
 try {
-  module.exports = require('hyperx')(require('hyperdom').jsx)
-}
-catch(e) {
-  if (e.code == 'MODULE_NOT_FOUND' && e.message.indexOf('hyperx') != -1) {
+  var hyperx = require('hyperx')
+} catch(e) {
+  if (e.code == 'MODULE_NOT_FOUND') {
     throw new Error('to use hyperx with hyperdom you need to install the hyperx package')
   }
   throw e
 }
+
+module.exports = hyperx(require('./rendering').jsx)
