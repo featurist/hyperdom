8196d079d00789776ffcabbf651475e5ee26408a
artemave <artemave@gmail.com>
2018-12-04 19:08:57 +0100

    Fix (fudge?) svg tsx types

--- index.d.ts
+++ index.d.ts
@@ -113,6 +113,294 @@ export = hyperdom
 
 declare global {
   namespace JSX {
+    interface WebViewHTMLAttributes extends HTMLElement {
+      allowFullScreen?: boolean
+      allowpopups?: boolean
+      autoFocus?: boolean
+      autosize?: boolean
+      blinkfeatures?: string
+      disableblinkfeatures?: string
+      disableguestresize?: boolean
+      disablewebsecurity?: boolean
+      guestinstance?: string
+      httpreferrer?: string
+      nodeintegration?: boolean
+      partition?: string
+      plugins?: boolean
+      preload?: string
+      src?: string
+      useragent?: string
+      webpreferences?: string
+    }
+
+    interface SVGAttributes {
+      // Attributes which also defined in HTMLAttributes
+      // See comment in SVGDOMPropertyConfig.js
+      className?: string
+      color?: string
+      height?: number | string
+      id?: string
+      lang?: string
+      max?: number | string
+      media?: string
+      method?: string
+      min?: number | string
+      name?: string
+      target?: string
+      type?: string
+      width?: number | string
+
+      // Other HTML properties supported by SVG elements in browsers
+      role?: string
+      tabIndex?: number
+
+      // SVG Specific attributes
+      accentHeight?: number | string
+      accumulate?: "none" | "sum"
+      additive?: "replace" | "sum"
+      alignmentBaseline?: "auto" | "baseline" | "before-edge" | "text-before-edge" |
+        "middle" | "central" | "after-edge" |
+        "text-after-edge" | "ideographic" | "alphabetic" | "hanging" | "mathematical" | "inherit"
+      allowReorder?: "no" | "yes"
+      alphabetic?: number | string
+      amplitude?: number | string
+      arabicForm?: "initial" | "medial" | "terminal" | "isolated"
+      ascent?: number | string
+      attributeName?: string
+      attributeType?: string
+      autoReverse?: number | string
+      azimuth?: number | string
+      baseFrequency?: number | string
+      baselineShift?: number | string
+      baseProfile?: number | string
+      bbox?: number | string
+      begin?: number | string
+      bias?: number | string
+      by?: number | string
+      calcMode?: number | string
+      capHeight?: number | string
+      clip?: number | string
+      clipPath?: string
+      clipPathUnits?: number | string
+      clipRule?: number | string
+      colorInterpolation?: number | string
+      colorInterpolationFilters?: "auto" | "sRGB" | "linearRGB" | "inherit"
+      colorProfile?: number | string
+      colorRendering?: number | string
+      contentScriptType?: number | string
+      contentStyleType?: number | string
+      cursor?: number | string
+      cx?: number | string
+      cy?: number | string
+      d?: string
+      decelerate?: number | string
+      descent?: number | string
+      diffuseConstant?: number | string
+      direction?: number | string
+      display?: number | string
+      divisor?: number | string
+      dominantBaseline?: number | string
+      dur?: number | string
+      dx?: number | string
+      dy?: number | string
+      edgeMode?: number | string
+      elevation?: number | string
+      enableBackground?: number | string
+      end?: number | string
+      exponent?: number | string
+      externalResourcesRequired?: number | string
+      fill?: string
+      fillOpacity?: number | string
+      fillRule?: "nonzero" | "evenodd" | "inherit"
+      filter?: string
+      filterRes?: number | string
+      filterUnits?: number | string
+      floodColor?: number | string
+      floodOpacity?: number | string
+      focusable?: number | string
+      fontFamily?: string
+      fontSize?: number | string
+      fontSizeAdjust?: number | string
+      fontStretch?: number | string
+      fontStyle?: number | string
+      fontVariant?: number | string
+      fontWeight?: number | string
+      format?: number | string
+      from?: number | string
+      fx?: number | string
+      fy?: number | string
+      g1?: number | string
+      g2?: number | string
+      glyphName?: number | string
+      glyphOrientationHorizontal?: number | string
+      glyphOrientationVertical?: number | string
+      glyphRef?: number | string
+      gradientTransform?: string
+      gradientUnits?: string
+      hanging?: number | string
+      horizAdvX?: number | string
+      horizOriginX?: number | string
+      href?: string
+      ideographic?: number | string
+      imageRendering?: number | string
+      in2?: number | string
+      in?: string
+      intercept?: number | string
+      k1?: number | string
+      k2?: number | string
+      k3?: number | string
+      k4?: number | string
+      k?: number | string
+      kernelMatrix?: number | string
+      kernelUnitLength?: number | string
+      kerning?: number | string
+      keyPoints?: number | string
+      keySplines?: number | string
+      keyTimes?: number | string
+      lengthAdjust?: number | string
+      letterSpacing?: number | string
+      lightingColor?: number | string
+      limitingConeAngle?: number | string
+      local?: number | string
+      markerEnd?: string
+      markerHeight?: number | string
+      markerMid?: string
+      markerStart?: string
+      markerUnits?: number | string
+      markerWidth?: number | string
+      mask?: string
+      maskContentUnits?: number | string
+      maskUnits?: number | string
+      mathematical?: number | string
+      mode?: number | string
+      numOctaves?: number | string
+      offset?: number | string
+      opacity?: number | string
+      operator?: number | string
+      order?: number | string
+      orient?: number | string
+      orientation?: number | string
+      origin?: number | string
+      overflow?: number | string
+      overlinePosition?: number | string
+      overlineThickness?: number | string
+      paintOrder?: number | string
+      panose1?: number | string
+      pathLength?: number | string
+      patternContentUnits?: string
+      patternTransform?: number | string
+      patternUnits?: string
+      pointerEvents?: number | string
+      points?: string
+      pointsAtX?: number | string
+      pointsAtY?: number | string
+      pointsAtZ?: number | string
+      preserveAlpha?: number | string
+      preserveAspectRatio?: string
+      primitiveUnits?: number | string
+      r?: number | string
+      radius?: number | string
+      refX?: number | string
+      refY?: number | string
+      renderingIntent?: number | string
+      repeatCount?: number | string
+      repeatDur?: number | string
+      requiredExtensions?: number | string
+      requiredFeatures?: number | string
+      restart?: number | string
+      result?: string
+      rotate?: number | string
+      rx?: number | string
+      ry?: number | string
+      scale?: number | string
+      seed?: number | string
+      shapeRendering?: number | string
+      slope?: number | string
+      spacing?: number | string
+      specularConstant?: number | string
+      specularExponent?: number | string
+      speed?: number | string
+      spreadMethod?: string
+      startOffset?: number | string
+      stdDeviation?: number | string
+      stemh?: number | string
+      stemv?: number | string
+      stitchTiles?: number | string
+      stopColor?: string
+      stopOpacity?: number | string
+      strikethroughPosition?: number | string
+      strikethroughThickness?: number | string
+      string?: number | string
+      stroke?: string
+      strokeDasharray?: string | number
+      strokeDashoffset?: string | number
+      strokeLinecap?: "butt" | "round" | "square" | "inherit"
+      strokeLinejoin?: "miter" | "round" | "bevel" | "inherit"
+      strokeMiterlimit?: number | string
+      strokeOpacity?: number | string
+      strokeWidth?: number | string
+      surfaceScale?: number | string
+      systemLanguage?: number | string
+      tableValues?: number | string
+      targetX?: number | string
+      targetY?: number | string
+      textAnchor?: string
+      textDecoration?: number | string
+      textLength?: number | string
+      textRendering?: number | string
+      to?: number | string
+      transform?: string
+      u1?: number | string
+      u2?: number | string
+      underlinePosition?: number | string
+      underlineThickness?: number | string
+      unicode?: number | string
+      unicodeBidi?: number | string
+      unicodeRange?: number | string
+      unitsPerEm?: number | string
+      vAlphabetic?: number | string
+      values?: string
+      vectorEffect?: number | string
+      version?: string
+      vertAdvY?: number | string
+      vertOriginX?: number | string
+      vertOriginY?: number | string
+      vHanging?: number | string
+      vIdeographic?: number | string
+      viewBox?: string
+      viewTarget?: number | string
+      visibility?: number | string
+      vMathematical?: number | string
+      widths?: number | string
+      wordSpacing?: number | string
+      writingMode?: number | string
+      x1?: number | string
+      x2?: number | string
+      x?: number | string
+      xChannelSelector?: string
+      xHeight?: number | string
+      xlinkActuate?: string
+      xlinkArcrole?: string
+      xlinkHref?: string
+      xlinkRole?: string
+      xlinkShow?: string
+      xlinkTitle?: string
+      xlinkType?: string
+      xmlBase?: string
+      xmlLang?: string
+      xmlns?: string
+      xmlnsXlink?: string
+      xmlSpace?: string
+      y1?: number | string
+      y2?: number | string
+      y?: number | string
+      yChannelSelector?: string
+      z?: number | string
+      zoomAndPan?: string
+    }
+
+    type HyperdomSVGAttributes<T> = SVGAttributes & Exclude<SVGAttributes, T> & hyperdom.HyperdomNodeProps
+
     interface IntrinsicElements {
       // HTML & hyperdom.HyperdomNodeProps
       a: Partial<HTMLAnchorElement> & hyperdom.HyperdomNodeProps
@@ -229,65 +517,63 @@ declare global {
       "var": Partial<HTMLElement> & hyperdom.HyperdomNodeProps
       video: Partial<HTMLVideoElement> & hyperdom.HyperdomNodeProps
       wbr: Partial<HTMLElement> & hyperdom.HyperdomNodeProps
-      webview: Partial<HTMLElement> & hyperdom.HyperdomNodeProps
- & hyperdom.HyperdomNodeProps
+      webview: Partial<WebViewHTMLAttributes> & hyperdom.HyperdomNodeProps
       // SVG
-      svg: Partial<SVGSVGElement> & hyperdom.HyperdomNodeProps
- & hyperdom.HyperdomNodeProps
-      animate: Partial<SVGElement> & hyperdom.HyperdomNodeProps
-      animateTransform: Partial<SVGElement> & hyperdom.HyperdomNodeProps
-      circle: Partial<SVGCircleElement> & hyperdom.HyperdomNodeProps
-      clipPath: Partial<SVGClipPathElement> & hyperdom.HyperdomNodeProps
-      defs: Partial<SVGDefsElement> & hyperdom.HyperdomNodeProps
-      desc: Partial<SVGDescElement> & hyperdom.HyperdomNodeProps
-      ellipse: Partial<SVGEllipseElement> & hyperdom.HyperdomNodeProps
-      feBlend: Partial<SVGFEBlendElement> & hyperdom.HyperdomNodeProps
-      feColorMatrix: Partial<SVGFEColorMatrixElement> & hyperdom.HyperdomNodeProps
-      feComponentTransfer: Partial<SVGFEComponentTransferElement> & hyperdom.HyperdomNodeProps
-      feComposite: Partial<SVGFECompositeElement> & hyperdom.HyperdomNodeProps
-      feConvolveMatrix: Partial<SVGFEConvolveMatrixElement> & hyperdom.HyperdomNodeProps
-      feDiffuseLighting: Partial<SVGFEDiffuseLightingElement> & hyperdom.HyperdomNodeProps
-      feDisplacementMap: Partial<SVGFEDisplacementMapElement> & hyperdom.HyperdomNodeProps
-      feDistantLight: Partial<SVGFEDistantLightElement> & hyperdom.HyperdomNodeProps
-      feFlood: Partial<SVGFEFloodElement> & hyperdom.HyperdomNodeProps
-      feFuncA: Partial<SVGFEFuncAElement> & hyperdom.HyperdomNodeProps
-      feFuncB: Partial<SVGFEFuncBElement> & hyperdom.HyperdomNodeProps
-      feFuncG: Partial<SVGFEFuncGElement> & hyperdom.HyperdomNodeProps
-      feFuncR: Partial<SVGFEFuncRElement> & hyperdom.HyperdomNodeProps
-      feGaussianBlur: Partial<SVGFEGaussianBlurElement> & hyperdom.HyperdomNodeProps
-      feImage: Partial<SVGFEImageElement> & hyperdom.HyperdomNodeProps
-      feMerge: Partial<SVGFEMergeElement> & hyperdom.HyperdomNodeProps
-      feMergeNode: Partial<SVGFEMergeNodeElement> & hyperdom.HyperdomNodeProps
-      feMorphology: Partial<SVGFEMorphologyElement> & hyperdom.HyperdomNodeProps
-      feOffset: Partial<SVGFEOffsetElement> & hyperdom.HyperdomNodeProps
-      fePointLight: Partial<SVGFEPointLightElement> & hyperdom.HyperdomNodeProps
-      feSpecularLighting: Partial<SVGFESpecularLightingElement> & hyperdom.HyperdomNodeProps
-      feSpotLight: Partial<SVGFESpotLightElement> & hyperdom.HyperdomNodeProps
-      feTile: Partial<SVGFETileElement> & hyperdom.HyperdomNodeProps
-      feTurbulence: Partial<SVGFETurbulenceElement> & hyperdom.HyperdomNodeProps
-      filter: Partial<SVGFilterElement> & hyperdom.HyperdomNodeProps
-      foreignObject: Partial<SVGForeignObjectElement> & hyperdom.HyperdomNodeProps
-      g: Partial<SVGGElement> & hyperdom.HyperdomNodeProps
-      image: Partial<SVGImageElement> & hyperdom.HyperdomNodeProps
-      line: Partial<SVGLineElement> & hyperdom.HyperdomNodeProps
-      linearGradient: Partial<SVGLinearGradientElement> & hyperdom.HyperdomNodeProps
-      marker: Partial<SVGMarkerElement> & hyperdom.HyperdomNodeProps
-      mask: Partial<SVGMaskElement> & hyperdom.HyperdomNodeProps
-      metadata: Partial<SVGMetadataElement> & hyperdom.HyperdomNodeProps
-      path: Partial<SVGPathElement> & hyperdom.HyperdomNodeProps
-      pattern: Partial<SVGPatternElement> & hyperdom.HyperdomNodeProps
-      polygon: Partial<SVGPolygonElement> & hyperdom.HyperdomNodeProps
-      polyline: Partial<SVGPolylineElement> & hyperdom.HyperdomNodeProps
-      radialGradient: Partial<SVGRadialGradientElement> & hyperdom.HyperdomNodeProps
-      rect: Partial<SVGRectElement> & hyperdom.HyperdomNodeProps
-      stop: Partial<SVGStopElement> & hyperdom.HyperdomNodeProps
-      switch: Partial<SVGSwitchElement> & hyperdom.HyperdomNodeProps
-      symbol: Partial<SVGSymbolElement> & hyperdom.HyperdomNodeProps
-      text: Partial<SVGTextElement> & hyperdom.HyperdomNodeProps
-      textPath: Partial<SVGTextPathElement> & hyperdom.HyperdomNodeProps
-      tspan: Partial<SVGTSpanElement> & hyperdom.HyperdomNodeProps
-      use: Partial<SVGUseElement> & hyperdom.HyperdomNodeProps
-      view: Partial<SVGViewElement> & hyperdom.HyperdomNodeProps
+      svg: HyperdomSVGAttributes<SVGSVGElement>
+      animate: HyperdomSVGAttributes<SVGElement>
+      animateTransform: HyperdomSVGAttributes<SVGElement>
+      circle: HyperdomSVGAttributes<SVGCircleElement>
+      clipPath: HyperdomSVGAttributes<SVGClipPathElement>
+      defs: HyperdomSVGAttributes<SVGDefsElement>
+      desc: HyperdomSVGAttributes<SVGDescElement>
+      ellipse: HyperdomSVGAttributes<SVGEllipseElement>
+      feBlend: HyperdomSVGAttributes<SVGFEBlendElement>
+      feColorMatrix: HyperdomSVGAttributes<SVGFEColorMatrixElement>
+      feComponentTransfer: HyperdomSVGAttributes<SVGFEComponentTransferElement>
+      feComposite: HyperdomSVGAttributes<SVGFECompositeElement>
+      feConvolveMatrix: HyperdomSVGAttributes<SVGFEConvolveMatrixElement>
+      feDiffuseLighting: HyperdomSVGAttributes<SVGFEDiffuseLightingElement>
+      feDisplacementMap: HyperdomSVGAttributes<SVGFEDisplacementMapElement>
+      feDistantLight: HyperdomSVGAttributes<SVGFEDistantLightElement>
+      feFlood: HyperdomSVGAttributes<SVGFEFloodElement>
+      feFuncA: HyperdomSVGAttributes<SVGFEFuncAElement>
+      feFuncB: HyperdomSVGAttributes<SVGFEFuncBElement>
+      feFuncG: HyperdomSVGAttributes<SVGFEFuncGElement>
+      feFuncR: HyperdomSVGAttributes<SVGFEFuncRElement>
+      feGaussianBlur: HyperdomSVGAttributes<SVGFEGaussianBlurElement>
+      feImage: HyperdomSVGAttributes<SVGFEImageElement>
+      feMerge: HyperdomSVGAttributes<SVGFEMergeElement>
+      feMergeNode: HyperdomSVGAttributes<SVGFEMergeNodeElement>
+      feMorphology: HyperdomSVGAttributes<SVGFEMorphologyElement>
+      feOffset: HyperdomSVGAttributes<SVGFEOffsetElement>
+      fePointLight: HyperdomSVGAttributes<SVGFEPointLightElement>
+      feSpecularLighting: HyperdomSVGAttributes<SVGFESpecularLightingElement>
+      feSpotLight: HyperdomSVGAttributes<SVGFESpotLightElement>
+      feTile: HyperdomSVGAttributes<SVGFETileElement>
+      feTurbulence: HyperdomSVGAttributes<SVGFETurbulenceElement>
+      filter: HyperdomSVGAttributes<SVGFilterElement>
+      foreignObject: HyperdomSVGAttributes<SVGForeignObjectElement>
+      g: HyperdomSVGAttributes<SVGGElement>
+      image: HyperdomSVGAttributes<SVGImageElement>
+      line: HyperdomSVGAttributes<SVGLineElement>
+      linearGradient: HyperdomSVGAttributes<SVGLinearGradientElement>
+      marker: HyperdomSVGAttributes<SVGMarkerElement>
+      mask: HyperdomSVGAttributes<SVGMaskElement>
+      metadata: HyperdomSVGAttributes<SVGMetadataElement>
+      path: HyperdomSVGAttributes<SVGPathElement>
+      pattern: HyperdomSVGAttributes<SVGPatternElement>
+      polygon: HyperdomSVGAttributes<SVGPolygonElement>
+      polyline: HyperdomSVGAttributes<SVGPolylineElement>
+      radialGradient: HyperdomSVGAttributes<SVGRadialGradientElement>
+      rect: HyperdomSVGAttributes<SVGRectElement>
+      stop: HyperdomSVGAttributes<SVGStopElement>
+      switch: HyperdomSVGAttributes<SVGSwitchElement>
+      symbol: HyperdomSVGAttributes<SVGSymbolElement>
+      text: HyperdomSVGAttributes<SVGTextElement>
+      textPath: HyperdomSVGAttributes<SVGTextPathElement>
+      tspan: HyperdomSVGAttributes<SVGTSpanElement>
+      use: HyperdomSVGAttributes<SVGUseElement>
+      view: HyperdomSVGAttributes<SVGViewElement>
     }
   }
 }
--- test/browser/tsxSpec.tsx
+++ test/browser/tsxSpec.tsx
@@ -35,8 +35,19 @@ describe('tsx integration', function () {
             <li key="two">two</li>
             <li key="three">three</li>
           </ul>
+          <table width="100%">
+            <th colSpan={2}></th>
+          </table>
         </div>
       )
+    }
+
+    class Circle extends hyperdom.RenderComponent {
+      public render () {
+        return <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+          <circle cx="50" cy="50" r="40" stroke="red" stroke-width="4" fill="yellow" />
+        </svg>
+      }
     }
   })
 
